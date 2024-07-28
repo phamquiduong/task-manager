592ada4b924d4ad93d63fece43b0360dbead64fc
Pham Qui Duong <phamquiduong@outlook.com>
2024-07-28 22:23:26 +0700

    Update admin session

--- src/session/admin.py
+++ src/session/admin.py
@@ -5,8 +5,8 @@ from session import models
 
 class SessionAdmin(admin.ModelAdmin):
     list_display = ('user_id', 'user_agent', 'expired_datetime')
-    search_fields = ('user_id', 'user_agent', 'expired_datetime')
-    list_filter = ('user_id', 'user_agent', 'expired_datetime')
+    search_fields = ('user_id',)
+    list_filter = ('user_id',)
 
 
 admin.site.register(models.Session, SessionAdmin)
